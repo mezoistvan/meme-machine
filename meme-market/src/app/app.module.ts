c6e8b3b2063a8b109028e52f0d7a3ad063146951
mezoistvan <mezoistvan1@gmail.com>
2018-05-03 23:55:45 +0200

    update to v6. cursors v1 added. header footer added.

--- meme-market/src/app/app.module.ts
+++ meme-market/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowseComponent } from './browse/browse.component';
+import { CreateComponent } from './create/create.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'browse', pathMatch: 'full' },
+  { path: 'browse', component: BrowseComponent },
+  { path: 'create', component: CreateComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BrowseComponent,
+    CreateComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {initialNavigation: 'enabled'})
   ],
   providers: [],
   bootstrap: [AppComponent]
